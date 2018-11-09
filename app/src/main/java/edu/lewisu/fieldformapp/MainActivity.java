dad016ec66aeb4b449f7554c1e555c79c823fa02
soniHiren <hirensoni@lewisu.edu>
2018-11-09 16:55:28 -0600

    Updated MainActivity and SQLiteDatabase

--- app/src/main/java/edu/lewisu/fieldformapp/MainActivity.java
+++ app/src/main/java/edu/lewisu/fieldformapp/MainActivity.java
@@ -10,7 +10,10 @@ import android.content.Intent;
 
 public class MainActivity extends BaseActivity {
 
-    EditText firstName,Address;
+    EditText firstName, middleName, lastName, Address, City, State, Zip, County, dateOfBirth,
+            Gender, Ethnicity, SSNum, phoneNum, email, contactPref, highSchool,
+            gradYear, programOfInterest, extraCurricularActivities, Hobbies,
+            Scholarships, finanAid, medInfo, Consent;
     SQLDatabase sql;
 
     @Override
@@ -18,7 +21,29 @@ public class MainActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         firstName=(EditText)findViewById(R.id.editText);
-        Address=(EditText)findViewById(R.id.editText2);
+        middleName=(EditText)findViewById(R.id.editText);
+        lastName=(EditText)findViewById(R.id.editText);
+        Address=(EditText)findViewById(R.id.editText);
+        City=(EditText)findViewById(R.id.editText);
+        State=(EditText)findViewById(R.id.editText);
+        Zip=(EditText)findViewById(R.id.editText);
+        County=(EditText)findViewById(R.id.editText);
+        dateOfBirth=(EditText)findViewById(R.id.editText);
+        Gender=(EditText)findViewById(R.id.editText);
+        Ethnicity=(EditText)findViewById(R.id.editText);
+        SSNum=(EditText)findViewById(R.id.editText);
+        phoneNum=(EditText)findViewById(R.id.editText);
+        email=(EditText)findViewById(R.id.editText);
+        contactPref=(EditText)findViewById(R.id.editText);
+        highSchool=(EditText)findViewById(R.id.editText);
+        gradYear=(EditText)findViewById(R.id.editText);
+        programOfInterest=(EditText)findViewById(R.id.editText);
+        extraCurricularActivities=(EditText)findViewById(R.id.editText);
+        Hobbies=(EditText)findViewById(R.id.editText);
+        Scholarships=(EditText)findViewById(R.id.editText);
+        finanAid=(EditText)findViewById(R.id.editText);
+        medInfo=(EditText)findViewById(R.id.editText);
+        Consent=(EditText)findViewById(R.id.editText);
         sql=new SQLDatabase(MainActivity.this);
     }
 
@@ -26,9 +51,34 @@ public class MainActivity extends BaseActivity {
     public void click(View v)
     {
         String fName = firstName.getText().toString();
+        String mName = middleName.getText().toString();
+        String lName = lastName.getText().toString();
         String add = Address.getText().toString();
+        String city = City.getText().toString();
+        String state = State.getText().toString();
+        String zip = Zip.getText().toString();
+        String county = County.getText().toString();
+        String DOB = dateOfBirth.getText().toString();
+        String gender = Gender.getText().toString();
+        String ethnicity = Ethnicity.getText().toString();
+        String SSN = SSNum.getText().toString();
+        String PhoneNum = phoneNum.getText().toString();
+        String eMail = email.getText().toString();
+        String ContactPref = contactPref.getText().toString();
+        String HighSchool = highSchool.getText().toString();
+        String GradYear = gradYear.getText().toString();
+        String ProgramOfInterest = programOfInterest.getText().toString();
+        String ExtraCurricularActivities = extraCurricularActivities.getText().toString();
+        String hobbies = Hobbies.getText().toString();
+        String scholarships = Scholarships.getText().toString();
+        String FinanAid = finanAid.getText().toString();
+        String MedInfo = medInfo.getText().toString();
+        String consent = Consent.getText().toString();
         sql.open();
-        sql.save(fName, add);
+        sql.save(fName,mName, lName, add, city, state, zip, county, DOB, gender, ethnicity, SSN,
+                PhoneNum, eMail, ContactPref, HighSchool, GradYear,
+                ProgramOfInterest, ExtraCurricularActivities, hobbies,
+                scholarships, FinanAid, MedInfo, consent);
         sql.close();
         Toast.makeText(MainActivity.this, "Saved Successfully", Toast.LENGTH_SHORT).show();
 
--- app/src/main/java/edu/lewisu/fieldformapp/SQLDatabase.java
+++ app/src/main/java/edu/lewisu/fieldformapp/SQLDatabase.java
@@ -11,33 +11,32 @@ public class SQLDatabase {
     String table="user";
     int version=1;
 
-    String firstName="name";
-    String Address="address";
+    String firstName="First Name";
+    String middleName="Middle Name";
+    String lastName="Last Name";
+    String Address="Address";
+    String City="City";
+    String State="State";
+    String Zip="Zip Code";
+    String County="County";
+    String dateOfBirth="Date Of Birth";
+    String Gender="Gender";
+    String Ethnicity="Ethnicity";
+    String SSNum="Social Security Number";
+    String phoneNum="Phone Number";
+    String email="Email Address";
+    String contactPref="Contact Preference";
+    String highSchool="High School";
+    String gradYear="High School Graduation Year";
+    String programOfInterest="Program of Interest";
+    String extraCurricularActivities="Extra Curricular Activities";
+    String Hobbies="Hobbies";
+    String Scholarships="Scholarships";
+    String finanAid="Financial Aid";
+    String medInfo="Medical Information";
+    String Consent ="Consent";
     String id="id";
 
-
-//    String middleName="Middle Name";
-//    String lastName="Last Name";
-//    String dateOfBirth="Date Of Birth";
-//    String SSN="Social Security Number";
-//    String email="Email Address";
-//    String phoneNum="Phone Number";
-//    String contactPref="Contact Preference";
-//    String City="City";
-//    String State="State";
-//    String Zip="Zip Code";
-//    String County="County";
-//    String Gender="Gender";
-//    String Ethnicity="Ethnicity";
-//    String Consent ="Consent";
-//    String highSchool="High School";
-//    String programOfInterest="Program of Interest";
-//    String gradYear="High School Graduation Year";
-//    String extraCurricularActivity="Extra Curricular Activities";
-//    String Hobbies="Hobbies";
-//    String Scholarships="Scholarships";
-//    String finanAid="Financial Aid";
-//    String medInfo="Medical Information";
 
     helper h;
     Context c;
@@ -57,19 +56,45 @@ public class SQLDatabase {
         s=h.getWritableDatabase();
     }
 
-    public void save(String na, String ad)
-    {
-        ContentValues cv=new ContentValues();
-        cv.put(firstName, na);
-        cv.put(Address, ad);
-        s.insert(table, null, cv);
-    }
-
     public void close()
     {
         s.close();
     }
 
+    public void save(String fName, String mName, String ln, String add, String city,
+                     String state, String zip, String county,
+                     String DOB, String gender, String ethnicity, String SSN,
+                     String PhoneNum, String eMail, String ContactPref, String HighSchool,
+                     String GradYear, String ProgramOfInterest, String ExtraCurricularActivities,
+                     String hobbies, String scholarships,
+                     String FinanAid, String MedInfo, String consent) {
+        ContentValues cv=new ContentValues();
+        cv.put(firstName, fName);
+        cv.put(middleName, mName);
+        cv.put(lastName, ln);
+        cv.put(Address, add);
+        cv.put(City, city);
+        cv.put(State, state);
+        cv.put(Zip, zip);
+        cv.put(County,county);
+        cv.put(dateOfBirth, DOB);
+        cv.put(Gender, gender);
+        cv.put(Ethnicity, ethnicity);
+        cv.put(SSNum, SSN);
+        cv.put(phoneNum, PhoneNum);
+        cv.put(email, eMail);
+        cv.put(contactPref, ContactPref);
+        cv.put(highSchool, HighSchool);
+        cv.put(gradYear, GradYear);
+        cv.put(programOfInterest, ProgramOfInterest);
+        cv.put(extraCurricularActivities, ExtraCurricularActivities);
+        cv.put(Hobbies, hobbies);
+        cv.put(Scholarships, scholarships);
+        cv.put(finanAid, FinanAid);
+        cv.put(medInfo, MedInfo);
+        cv.put(Consent, consent);
+        s.insert(table, null, cv);
+    }
 
     public class helper extends SQLiteOpenHelper {
         public helper(Context context) {
@@ -80,7 +105,18 @@ public class SQLDatabase {
         public void onCreate(SQLiteDatabase arg0) {
             String query = "CREATE TABLE " + table + " ( " + id
                     + " INTEGER PRIMARY KEY AUTOINCREMENT, " + firstName
-                    + " TEXT NOT NULL, " + Address + " TEXT NOT NULL );";
+                    + "TEXT NOT NULL, " + middleName + "TEXT NOT NULL," + lastName + "TEXT NOT NULL," + Address + "TEXT NOT NULL,"
+                    + City + "TEXT NOT NULL," + State + "TEXT NOT NULL," + Zip + "TEXT NOT NULL,"
+                    + County + "TEXT NOT NULL," + dateOfBirth + "TEXT NOT NULL,"
+                    + Gender + "TEXT NOT NULL," + Ethnicity + "TEXT NOT NULL,"
+                    + SSNum + "TEXT NOT NULL," + phoneNum + "TEXT NOT NULL,"
+                    + email + "TEXT NOT NULL," + contactPref + "TEXT NOT NULL,"
+                    + highSchool + "TEXT NOT NULL," + gradYear + "TEXT NOT NULL,"
+                    + programOfInterest + "TEXT NOT NULL,"
+                    + extraCurricularActivities + "TEXT NOT NULL,"
+                    + Hobbies + "TEXT NOT NULL," + Scholarships + "TEXT NOT NULL,"
+                    + finanAid + "TEXT NOT NULL,"
+                    + medInfo + "TEXT NOT NULL," + Consent + "TEXT NOT NULL,);";
             arg0.execSQL(query);
 
         }
@@ -101,7 +137,10 @@ public class SQLDatabase {
         h = new helper(c);
         s = h.getReadableDatabase();
         String txt = "";
-        String[] col = { id, firstName, Address };
+        String[] col = { id, firstName, middleName, lastName, Address, City, State, Zip,
+                County, dateOfBirth, Gender, Ethnicity, SSNum, phoneNum, email,
+                contactPref, highSchool, gradYear, programOfInterest,
+                extraCurricularActivities, Hobbies, Scholarships, finanAid, medInfo, Consent};
         Cursor c = s.query(table, col, null, null, null, null, null); //fetching data from database
         c.moveToFirst();
         for (c.moveToFirst(); !c.isAfterLast(); c.moveToNext()) {
